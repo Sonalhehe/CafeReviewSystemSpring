ffd16651abaf33606fa725afcea7d62cbe89d2c2
Sonal Singh <sonalsingh4010@gmail.com>
2025-08-25 15:09:20 +0530

    Update CafeController.java

--- src/main/java/com/cafe/controller/CafeController.java
+++ src/main/java/com/cafe/controller/CafeController.java
@@ -1,1 +1,21 @@
+package com.cafe.controller;
+
+import com.cafe.model.Cafe;
+import com.cafe.service.CafeService;
+import org.springframework.web.bind.annotation.*;
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/cafes")
+@CrossOrigin(origins = "*")
+public class CafeController {
+    private final CafeService service;
+    public CafeController(CafeService service) { this.service = service; }
+
+    @GetMapping
+    public List<Cafe> getAllCafes() { return service.getAll(); }
+
+    @PostMapping
+    public Cafe addCafe(@RequestBody Cafe cafe) { return service.save(cafe); }
+}
 
