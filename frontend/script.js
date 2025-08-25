dcbdce3be7ca9dbf76f0006348ebcfa316cea6d0
Sonal Singh <sonalsingh4010@gmail.com>
2025-08-25 15:21:51 +0530

    Update script.js

--- frontend/script.js
+++ frontend/script.js
@@ -1,1 +1,16 @@
+const api = "http://localhost:8080/api/cafes";
 
+fetch(api)
+  .then(res => res.json())
+  .then(data => {
+    const container = document.getElementById("cafes");
+    data.forEach(cafe => {
+      const div = document.createElement("div");
+      div.className = "cafe";
+      div.innerHTML = `<h3>${cafe.name}</h3>
+                       <p>Location: ${cafe.location}</p>
+                       <p>Cuisine: ${cafe.cuisine}</p>
+                       <p>Avg Cost: ₹${cafe.avgCost}</p>`;
+      container.appendChild(div);
+    });
+  });
