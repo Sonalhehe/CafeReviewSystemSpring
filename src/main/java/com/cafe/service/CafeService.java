dc62c130c7973a387e659cf52cdae7c98bf0aea8
Sonal Singh <sonalsingh4010@gmail.com>
2025-08-25 15:12:35 +0530

    Update CafeService.java

--- src/main/java/com/cafe/service/CafeService.java
+++ src/main/java/com/cafe/service/CafeService.java
@@ -1,1 +1,15 @@
+package com.cafe.service;
 
+import com.cafe.model.Cafe;
+import com.cafe.repository.CafeRepository;
+import org.springframework.stereotype.Service;
+import java.util.List;
+
+@Service
+public class CafeService {
+    private final CafeRepository repo;
+    public CafeService(CafeRepository repo) { this.repo = repo; }
+
+    public List<Cafe> getAll() { return repo.findAll(); }
+    public Cafe save(Cafe cafe) { return repo.save(cafe); }
+}
