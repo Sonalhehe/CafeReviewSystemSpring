a838f02c74868989c05871f828da694d8fda6dab
Sonal Singh <sonalsingh4010@gmail.com>
2025-08-25 15:18:56 +0530

    Update data.sql

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,1 +1,7 @@
+INSERT INTO CAFE (id, name, location, cuisine, avg_cost) VALUES
+(1, 'Brew Cafe', 'Kolkata', 'Coffee', 250),
+(2, 'Tea Tales', 'Delhi', 'Tea', 150);
 
+INSERT INTO REVIEW (id, reviewer, comment, rating, cafe_id) VALUES
+(1, 'Sonal', 'Great coffee!', 5, 1),
+(2, 'Abhishek', 'Nice tea but crowded', 3, 2);
