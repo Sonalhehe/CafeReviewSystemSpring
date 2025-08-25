cc7b3b0720c9be2cabdbce0a2a7a55a9c736196b
Sonal Singh <sonalsingh4010@gmail.com>
2025-08-25 15:13:08 +0530

    Update ReviewRepository.java

--- src/main/java/com/cafe/repository/ReviewRepository.java
+++ src/main/java/com/cafe/repository/ReviewRepository.java
@@ -1,1 +1,6 @@
+package com.cafe.repository;
 
+import com.cafe.model.Review;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ReviewRepository extends JpaRepository<Review, Long> {}
