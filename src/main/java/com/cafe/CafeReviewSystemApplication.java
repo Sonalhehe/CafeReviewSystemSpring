6726e10fcefe185079da0c13d05cca8ee1bdc5f3
Sonal Singh <sonalsingh4010@gmail.com>
2025-08-25 15:11:29 +0530

    Update CafeReviewSystemApplication.java

--- src/main/java/com/cafe/CafeReviewSystemApplication.java
+++ src/main/java/com/cafe/CafeReviewSystemApplication.java
@@ -1,1 +1,11 @@
+package com.cafe;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CafeReviewSystemApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(CafeReviewSystemApplication.class, args);
+    }
+}
