d05474479cd2c062a1856a3d6389e8271031ccaf
Sonal Singh <sonalsingh4010@gmail.com>
2025-08-25 15:10:01 +0530

    Update ReviewController.java

--- src/main/java/com/cafe/controller/ReviewController.java
+++ src/main/java/com/cafe/controller/ReviewController.java
@@ -1,1 +1,21 @@
 
+package com.cafe.controller;
+
+import com.cafe.model.Review;
+import com.cafe.service.ReviewService;
+import org.springframework.web.bind.annotation.*;
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/reviews")
+@CrossOrigin(origins = "*")
+public class ReviewController {
+    private final ReviewService service;
+    public ReviewController(ReviewService service) { this.service = service; }
+
+    @GetMapping
+    public List<Review> getAllReviews() { return service.getAll(); }
+
+    @PostMapping
+    public Review addReview(@RequestBody Review review) { return service.save(review); }
+}
